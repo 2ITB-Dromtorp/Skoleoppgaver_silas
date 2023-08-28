e13c007d81963b464693736b1fa0ab9b4d8d1d55
silsur1 <silass@viken.no>
2023-08-28 14:20:36 +0200

    liste med fargekode punkter

--- react_intro/react_intro/src/mybutton.js
+++ react_intro/react_intro/src/mybutton.js
@@ -1,5 +1,29 @@
 import './App.css';
 
+function Uselist() {
+
+    const products = [
+        { title: 'Cabbage', isFruit: false, id: 1 },
+        { title: 'Garlic', isFruit: false, id: 2 },
+        { title: 'Apple', isFruit: true, id: 3 },
+    ];
+
+    const listItems = products.map(product =>
+<li
+      key={product.id}
+      style={{
+        color: product.isFruit ? 'magenta' : 'darkgreen'
+      }}
+    >
+      {product.title}
+    </li>
+    );
+
+    return <ul>{listItems}</ul>
+
+}
+
+
 function MyButton() {
     return (
       <button onClick={SayHi} className='Button'>
@@ -7,16 +31,21 @@ function MyButton() {
       </button>
     );
   }
-  
+    
+
   export default function Mybutton() {
+
     return (
       <div>
         <h1>Welcome to my app</h1>
         <MyButton />
+        <Uselist />
       </div>
     );
   }
 
   function SayHi() {
-    console.log("hello world")
-  }
+    console.log("wuhuu Gravemaskin");
+
+    
+}
