f0f7d7d2ce291fd039b78a0ef81c9210a49bb2af
silsur1 <silass@viken.no>
2023-08-29 13:57:48 +0200

    knapper som teller sammen

--- react_intro/react_intro/src/App.js
+++ react_intro/react_intro/src/App.js
@@ -3,6 +3,15 @@ import './App.css';
 import Mybutton from './mybutton';
 import Profile from './imageEX';
 
+let isLoggedIn = false;
+let content;
+if (isLoggedIn == true) {
+  content = <h1>hi</h1>;
+} else {
+  content = <h1>bye</h1>;
+}
+
+
 function App() {
   return (
     <div className="App">
@@ -12,7 +21,8 @@ function App() {
         <Profile />
         <h1>react intro</h1>
         <Mybutton />
-        
+        {content}
+
 
       </header>
     </div>
--- react_intro/react_intro/src/mybutton.js
+++ react_intro/react_intro/src/mybutton.js
@@ -1,4 +1,6 @@
 import './App.css';
+import { useState } from 'react';
+
 
 function Uselist() {
 
@@ -9,7 +11,7 @@ function Uselist() {
     ];
 
     const listItems = products.map(product =>
-<li
+    <li
       key={product.id}
       style={{
         color: product.isFruit ? 'magenta' : 'darkgreen'
@@ -23,15 +25,50 @@ function Uselist() {
 
 }
 
+export default function MyApp() {
+  const [count, setCount] = useState(0);
 
-function MyButton() {
-    return (
-      <button onClick={SayHi} className='Button'>
-        I'm a button
-      </button>
-    );
+  function handleClick() {
+    setCount(count + 1);
   }
-    
+
+  return (
+    <div>
+      <h1>Counters that update together</h1>
+      <MyButton count={count} onClick={handleClick} />
+      <MyButton count={count} onClick={handleClick} />
+      <Uselist />
+    </div>
+  );
+}
+
+function MyButton({ count, onClick }) {
+  return (
+    <button onClick={onClick} className='Button'>
+      Clicked {count} times
+    </button>
+  );
+}
+
+
+
+/*
+function MyButton() {
+
+    const [count, setCount] = useState(0);
+
+    function handleClick() {
+        setCount(count + 1);
+    }
+
+    return (
+      <button onClick={handleClick} className='Button'>
+      Clicked {count} times
+      </button>
+    ); 
+
+    }
+   
 
   export default function Mybutton() {
 
@@ -39,13 +76,14 @@ function MyButton() {
       <div>
         <h1>Welcome to my app</h1>
         <MyButton />
+        <MyButton />
         <Uselist />
       </div>
     );
   }
 
-  function SayHi() {
-    console.log("wuhuu Gravemaskin");
-
-    
+    function SayHi() {
+        console.log("wuhuu Gravemaskin");
+    }
 }
+*/
