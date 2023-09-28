16f2edde41bf6c7cfd1167584fcc6aab568810c7
silsur1 <silass@viken.no>
2023-09-28 08:27:21 +0200

    del 1 av stegn saks papir

--- react_intro/react_intro/src/R_P_S.js
+++ react_intro/react_intro/src/R_P_S.js
@@ -1,23 +1,28 @@
-import { useState, useEffect } from 'react';
+import { waitFor } from '@testing-library/react';
+import { getValue, wait } from '@testing-library/user-event/dist/utils';
+import { useState, useEffect, useCallback, } from 'react';
 import ConfettiExplosion from 'react-confetti-explosion';
 
 export default function Rock_paper_scissors() {
 
-    const [PR_P_S, setPR_P_S] = useState();
-    const [CR_P_S, setCR_P_S] = useState();
+    const [PR_P_S, setPR_P_S] = useState(0);
+    const [CR_P_S, setCR_P_S] = useState(0);
+    const [Win, setWin] = useState(0);
+    const [Loss, setloss] = useState(0);
+    const [Tie, setTie] = useState(0);
 
     const [isExploding, setIsExploding] = useState(false);
 
+
     let player = "";
     let opponent = "";
-    let resault = "";
+
 
 
     const Rock = () => {
         setPR_P_S(1)
-        console.log(PR_P_S);
-        console.log("rock");
-        setIsExploding(true)       
+        setCR_P_S(randomNumberInRange(1, 3))
+        Fight();
     }
     const Paper = () => {
         setPR_P_S(2)
@@ -38,19 +43,24 @@ export default function Rock_paper_scissors() {
     };
 
     const Fight = () => {
-        setCR_P_S(randomNumberInRange(1, 3))
-        console.log(CR_P_S);
+
+        
         
         if (PR_P_S==1 && CR_P_S==1) {
-            resault = "its a tie"
+            setTie(Tie+1)
+            console.log("you tied for the", Tie, "time")
         } else if (PR_P_S==1 && CR_P_S==2){
-            resault = "its a loss"
+            setloss(Loss+1)
+            console.log("you lost for the", Loss, "time")
         } else if (PR_P_S==1 && CR_P_S==3){
-            resault = "its a win"
+            setWin(Win+1)
+            console.log("you won for the", Win, "time")
         }
     }
 
     useEffect(() => {
+
+
     
         const myInterval = setInterval(() => {
             setIsExploding(false)
@@ -61,6 +71,7 @@ export default function Rock_paper_scissors() {
     
     });
 
+    //forteller hva du valgte 
     if (PR_P_S == 1) {
         player = "you chose rock"; 
     } else if (PR_P_S == 2){
@@ -70,7 +81,7 @@ export default function Rock_paper_scissors() {
     } else{
         player = "you have not chosen anything yet"
     }
-
+    //forteller hva PC-en valgte
     if (CR_P_S == 1) {
         opponent = "your opponent chose rock"; 
     } else if (CR_P_S == 2){
@@ -78,7 +89,7 @@ export default function Rock_paper_scissors() {
     } else if (CR_P_S == 3){
         opponent = "your opponent chose scissors"
     } else{
-        opponent = "your opponent had an error"
+        opponent = "your opponent have not chosen anything yet or had an error"
     }
     
     
@@ -99,7 +110,11 @@ export default function Rock_paper_scissors() {
 
             <button onClick={Fight}> fight </button>
 
-            <p> {resault} </p>
+            <p> {opponent} </p>
+
+            <p>wins {Win} </p>
+            <p>losses {Loss} </p>
+            <p>ties {Tie} </p>
 
             {isExploding && <ConfettiExplosion />}
 
