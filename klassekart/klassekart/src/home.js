a91d894179e50d4d471e74773d254bfc1617666b
silsur1 <silass@viken.no>
2023-10-31 13:46:25 +0100

    tilfeldige plasser når refresh siden men ikke med trykk av knapp

--- klassekart/klassekart/src/home.js
+++ klassekart/klassekart/src/home.js
@@ -1,5 +1,6 @@
 import './App.css';
 import { useNavigate } from 'react-router-dom';
+import { useEffect } from 'react';
 import Elev from './elev';
 import json from './profiles';
 
@@ -7,7 +8,20 @@ export default function Home() {
 
     const navigate = useNavigate();
 
+    const students = ["Matheo", "Elias", "Johannes", "Kristoffer", "Vetle", "Axel", "Silas", "Alva", "Mattis", "Theodor", "Gabriel", "-", "Philip", "Ahmad", "Andreas"]
+
+    const shuffleArray = (array) => { 
+      for (let i = array.length - 1; i > 0; i--) { 
+        const j = Math.floor(Math.random() * (i + 1)); 
+        [array[i], array[j]] = [array[j], array[i]]; 
+      } 
+      return array;
+    };  
+
+    shuffleArray(students); 
+   
     function swap() {
+
       
     }
 
@@ -16,60 +30,58 @@ export default function Home() {
 
       <div className="container">
 
-        <button onClick={swap}> bytt plasser </button>
+
+        <div className='leftside'>
+
+            <div className='box'>
+
+            <div className='tresitteplasser'> 
+                <Elev name= {students[0]} />
+                <Elev name= {students[1]}/>
+                <Elev name= {students[2]}/>
+              </div>
+
+              <div className='tresitteplasser'> 
+                <Elev name= {students[3]}/>
+                <Elev name= {students[4]}/>
+                <Elev name= {students[5]}/>
+              </div>
+
+              <div className='tresitteplasser'> 
+                <Elev name= {students[6]}/>
+                <Elev name= {students[7]}/>
+                <Elev name= {students[8]}/>
+              </div>
+
+              <button onClick={swap}> bytt plasser </button>
+
+            </div>
+          </div>
+
 
         <div className='rightside'>
 
         <div className='box'>
 
             <div className='tositteplasser'> 
-                <Elev name="Theodor"/>
-                <Elev name="Gabriel"/>
+                <Elev name= {students[9]}/>
+                <Elev name= {students[10]}/>
           </div>
 
           <div className='tositteplasser'> 
-                <Elev />
-                <Elev name="Philip"/>
+                <Elev name= {students[11]} />
+                <Elev name= {students[12]}/>
           </div>
 
           <div className='tositteplasser'> 
-                <Elev name="Ahmad"/>
-                <Elev name="Andreas"/>
+                <Elev name= {students[13]}/>
+                <Elev name= {students[14]}/>
           </div>
 
           </div>
 
           </div>        
-        
-        <div className='leftside'>
-
-            <div className='box'>
-
-            <div className='tresitteplasser'> 
-                <Elev name="Matheo"/>
-                <Elev name="Elias"/>
-                <Elev name="Johannes"/>
-              </div>
-
-              <div className='tresitteplasser'> 
-                <Elev name="Kristoffer"/>
-                <Elev name="Vetle"/>
-                <Elev name="Axel"/>
-              </div>
-
-              <div className='tresitteplasser'> 
-                <Elev name="Silas"/>
-                <Elev name="Alva"/>
-                <Elev name="Mattis"/>
-              </div>
-
-
-            </div>
-          </div>
-            
-
-
-
+      
 
         </div>
 
