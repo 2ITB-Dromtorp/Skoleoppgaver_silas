8eaa15aff3e404844839b2cb791c659d95f78e7f
silsur1 <silass@viken.no>
2023-11-14 13:39:49 +0100

    satt in database

--- nodeApplications/helloWorld/index.js
+++ nodeApplications/helloWorld/index.js
@@ -1,9 +1,34 @@
 const express = require('express')
 const app = express()
 const port = 3000
+var mysql = require('mysql');
+
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  port: 3306,
+  user     : 'root',
+  password : 'root',
+  database : 'droomtorp'
+});
+ 
+connection.connect(function(err) {
+  if (err) {
+    console.error('error connecting: ' + err.stack);
+    return;
+  }
+ 
+  console.log('connected as id ' + connection.threadId);
+});
+ 
+connection.query('SELECT * FROM elev', function (error, results, fields) {
+  if (error) throw error;
+  console.log('The solution is: ', results);
+});
+ 
+connection.end();
 
 app.get('/', (req, res) => {
-  res.send('Hello World!!!')
+  res.send('Hello World!!! I will destroy your sofa >:)')
 })
 
 app.listen(port, () => {
